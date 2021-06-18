be6cf0765c1a56dfe86caaa2b299745729aaac26
Chao Ly <lychaos123@gmail.com>
2021-06-18 17:09:41 -0500

    add homework assignment into the planner-jquery folder

---
+++ Develop/java.js
@@ -0,0 +1,22 @@
+var today = moment().format("MMMM Do, YYYY");
+$("#currentDay").text(today)
+
+var container = $(".container")
+
+var arrTime = ["9am", "10am", "11am", "12pm"];
+
+for(i=0; i<arrTime.length; i++){
+    var newRow = $("<div>").attr("class", "row")
+    var newDiv = $("<div>").attr("class", "col-2")
+    var newP = $("<p>").text(arrTime[i])
+
+    newDiv.append(newP)
+    var newTextA = $("<textarea>").attr("class", "col-8 present").attr("rows", "4")
+    var newBtn = $("<button>").attr("class", "col-2 saveBtn").text("Save")
+
+    newRow.append(newDiv)
+    newRow.append(newTextA)
+    newRow.append(newBtn)
+
+    container.append(newRow)
+}
