1dcec563ad6ef867664928b9518d79d1ed95ea04
Chao Ly <lychaos123@gmail.com>
2021-06-21 20:18:55 -0500

    take out the 12, 1, 2, 3, 4, 5pm  var arrTime so that i can add the right background color.

--- Develop/java.js
+++ Develop/java.js
@@ -3,7 +3,25 @@ $("#currentDay").text(today)
 
 var container = $(".container")
 
-var arrTime = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var arrTime = ["9am", "10am", "11am",];
+
+for (i = 0; i < arrTime.length; i++) {
+    var newRow = $("<div>").attr("class", "row")
+    var newDiv = $("<div>").attr("class", "col-2")
+    var newP = $("<p>").text(arrTime[i])
+
+    newDiv.append(newP)
+    var newTextA = $("<textarea>").attr("class", "col-8 past").attr("rows", "4")
+    var newBtn = $("<button>").attr("class", "col-2 saveBtn").text("Save")
+
+    newRow.append(newDiv)
+    newRow.append(newTextA)
+    newRow.append(newBtn)
+
+    container.append(newRow)
+}
+
+var arrTime = ["12pm"];
 
 for (i = 0; i < arrTime.length; i++) {
     var newRow = $("<div>").attr("class", "row")
