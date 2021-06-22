b438c950e2cf7569b44aa6855203eb342ace4d15
Chao Ly <lychaos123@gmail.com>
2021-06-21 20:26:57 -0500

    added a new arryTime for the 1, 2, 3, 4, 5pm and also add line 50 instead of present, i added future which change the color to green not red.

--- Develop/java.js
+++ Develop/java.js
@@ -38,3 +38,21 @@ for (i = 0; i < arrTime.length; i++) {
 
     container.append(newRow)
 }
+
+var arrTime = ["1pm", "2pm", "3pm", "4pm", "5pm"];
+
+for (i = 0; i < arrTime.length; i++) {
+    var newRow = $("<div>").attr("class", "row")
+    var newDiv = $("<div>").attr("class", "col-2")
+    var newP = $("<p>").text(arrTime[i])
+
+    newDiv.append(newP)
+    var newTextA = $("<textarea>").attr("class", "col-8 future").attr("rows", "4")
+    var newBtn = $("<button>").attr("class", "col-2 saveBtn").text("Save")
+
+    newRow.append(newDiv)
+    newRow.append(newTextA)
+    newRow.append(newBtn)
+
+    container.append(newRow)
+}
